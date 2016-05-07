6338c1059251c77f01d933cc93afe936b292ed4b
bobjamin <xbobjamin@gmail.com>
2016-05-07 23:05:49 +0100

    Boot app

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
-/**
- * Created by n0237573 on 5/7/16.
- */
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@Controller
 public class Main {
 
-    public static void main(String[] args){
-        System.err.println("Hello World from Java 8!");
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
     }
 }
